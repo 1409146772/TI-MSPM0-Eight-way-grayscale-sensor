e5c3ab2cd035199b3cb68ed3f6e0e8c70d91cf82
Ziyu LU <1409146772@qq.com>
2025-12-13 17:09:45 +0800

    串口引脚适配天猛星

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -8,14 +8,14 @@ SHELL = cmd.exe
 %.o: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"D:/MCU/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.3.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/LuZIyu/workspace_ccstheia/huidu_yjian" -I"C:/Users/LuZIyu/workspace_ccstheia/huidu_yjian/Debug" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source/third_party/CMSIS/Core/Include" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source" -gdwarf-3 -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"D:/MCU/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.3.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/LuZIyu/workspace_ccstheia/8-channel grayscale sensor" -I"C:/Users/LuZIyu/workspace_ccstheia/8-channel grayscale sensor/Debug" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source/third_party/CMSIS/Core/Include" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source" -gdwarf-3 -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
 build-1225677504: ../empty.syscfg
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: SysConfig'
-	"D:/MCU/CCS/ccs/utils/sysconfig_1.24.0/sysconfig_cli.bat" -s "D:/MCU/CCS/mspm0_sdk_2_05_01_00/.metadata/product.json" -d "MSPM0G350X" -p "LQFP-48(PT)" -r "Default" --script "C:/Users/LuZIyu/workspace_ccstheia/huidu_yjian/empty.syscfg" --context "system" -o "." --compiler ticlang
+	"D:/MCU/CCS/ccs/utils/sysconfig_1.24.0/sysconfig_cli.bat" -s "D:/MCU/CCS/mspm0_sdk_2_05_01_00/.metadata/product.json" -d "MSPM0G350X" -p "LQFP-48(PT)" -r "Default" --script "C:/Users/LuZIyu/workspace_ccstheia/8-channel grayscale sensor/empty.syscfg" --context "system" -o "." --compiler ticlang
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
@@ -29,14 +29,14 @@ Event.dot: build-1225677504
 %.o: ./%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"D:/MCU/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.3.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/LuZIyu/workspace_ccstheia/huidu_yjian" -I"C:/Users/LuZIyu/workspace_ccstheia/huidu_yjian/Debug" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source/third_party/CMSIS/Core/Include" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source" -gdwarf-3 -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"D:/MCU/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.3.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/LuZIyu/workspace_ccstheia/8-channel grayscale sensor" -I"C:/Users/LuZIyu/workspace_ccstheia/8-channel grayscale sensor/Debug" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source/third_party/CMSIS/Core/Include" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source" -gdwarf-3 -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
 startup_mspm0g350x_ticlang.o: D:/MCU/CCS/mspm0_sdk_2_05_01_00/source/ti/devices/msp/m0p/startup_system_files/ticlang/startup_mspm0g350x_ticlang.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: Arm Compiler'
-	"D:/MCU/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.3.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/LuZIyu/workspace_ccstheia/huidu_yjian" -I"C:/Users/LuZIyu/workspace_ccstheia/huidu_yjian/Debug" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source/third_party/CMSIS/Core/Include" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source" -gdwarf-3 -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	"D:/MCU/CCS/ccs/tools/compiler/ti-cgt-armllvm_4.0.3.LTS/bin/tiarmclang.exe" -c @"device.opt"  -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O2 -I"C:/Users/LuZIyu/workspace_ccstheia/8-channel grayscale sensor" -I"C:/Users/LuZIyu/workspace_ccstheia/8-channel grayscale sensor/Debug" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source/third_party/CMSIS/Core/Include" -I"D:/MCU/CCS/mspm0_sdk_2_05_01_00/source" -gdwarf-3 -MMD -MP -MF"$(basename $(<F)).d_raw" -MT"$(@)"  $(GEN_OPTS__FLAG) -o"$@" "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
